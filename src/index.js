66a416adc44de924356e9a4f311e8c784ab79d37
Martin Svejda <svejdam13@gmail.com>
2020-10-16 20:49:57 +0100

    update

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const Image = () => (
   <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt=""/>
 );
 const Title = () => <h1>I Love You to the Moon and Back</h1>
-const Author = () => <h4>Amelia Hepworth</h4>
+const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Amelia Hepworth</h4>//another JSX syntax for CSS (overwrites the index.css!!)
 
 
 
